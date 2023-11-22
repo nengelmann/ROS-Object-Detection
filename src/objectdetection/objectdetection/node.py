6d02882ed02f4c942cb5afc6d9a7c9781651ae68
nengelmann <mail@nico-engelmann.de>
2023-11-22 11:06:06 +0100

    formatting and docstrings

--- src/objectdetection/objectdetection/node.py
+++ src/objectdetection/objectdetection/node.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python3
 
 import rclpy
+from cv_bridge import CvBridge
+from foxglove_msgs.msg import (
+    Color,
+    ImageAnnotations,
+    Point2,
+    PointsAnnotation,
+    TextAnnotation,
+)
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-
-from foxglove_msgs.msg import TextAnnotation, Point2, Color, ImageAnnotations, PointsAnnotation
 
 from .model import YOLOv3
 
+
 class ObjectDetectionNode(Node):
+    """ROS2 node for object detection with YOLOv3"""
+
     def __init__(self, min_bbox_area=400, min_bbox_side=25, debug=False):
-        super().__init__('objectdetection_node')
-        self.publisher_ = self.create_publisher(ImageAnnotations, '/d455_1_rgb_image/detected_objects', 10) # was: Detections, Detection2DArray, ImageMarkerArray
+        super().__init__("objectdetection_node")
+        self.publisher_ = self.create_publisher(
+            ImageAnnotations, "/d455_1_rgb_image/detected_objects", 10
+        )  # was: Detections, Detection2DArray, ImageMarkerArray
         self.subscription = self.create_subscription(
-            Image,
-            '/d455_1_rgb_image',
-            self.listener_callback,
-            10)
+            Image, "/d455_1_rgb_image", self.listener_callback, 10
+        )
         self.subscription  # prevent unused variable warning
         self.bridge = CvBridge()
         self.debug = False
 
-        self.model = YOLOv3(min_bbox_area=400, min_bbox_side=20, debug=self.debug)
-        
+        self.model = YOLOv3(
+            min_bbox_area=min_bbox_area,
+            min_bbox_side=min_bbox_side,
+            debug=self.debug,
+        )
+
         print("Object detection node started and active.")
 
     def listener_callback(self, msg):
+        """Process image with YOLOv3 and publish detected objects"""
         # Convert ROS Image message to OpenCV image
-        img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        timestamp=msg.header.stamp
+        img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        timestamp = msg.header.stamp
 
         # Process image with YOLOv3
         boxes, confidences, class_ids, class_names = self.model.detect(img)
@@ -39,67 +52,71 @@ class ObjectDetectionNode(Node):
 
         img_annotations = ImageAnnotations()
 
-        if len(boxes)>0:
+        if len(boxes) > 0:
             # Publish detected objects
             print(f"Detected objects: {class_names}")
 
-            for box, conf, class_name, class_id in zip(boxes, confidences, class_names, class_ids):
-
+            for box, conf, class_name, class_id in zip(
+                boxes, confidences, class_names, class_ids
+            ):
                 (x, y) = (box[0], box[1])
                 (w, h) = (box[2], box[3])
                 r, g, b = self.model.colors[class_id]
-                r, g, b = r/255., g/255., b/255.
+                r, g, b = r / 255.0, g / 255.0, b / 255.0
 
                 img_annotations.points.append(
                     PointsAnnotation(
                         timestamp=timestamp,
-                        type=3, #LINE_STRIP: 3
+                        type=3,  # LINE_STRIP: 3
                         points=[
                             Point2(x=float(x), y=float(y)),
-                            Point2(x=float(x+w), y=float(y)),
-                            Point2(x=float(x+w), y=float(y+h)),
-                            Point2(x=float(x), y=float(y+h)),
+                            Point2(x=float(x + w), y=float(y)),
+                            Point2(x=float(x + w), y=float(y + h)),
+                            Point2(x=float(x), y=float(y + h)),
                             Point2(x=float(x), y=float(y)),
                         ],
-                        outline_color=Color(a=1., b=b, g=g, r=r),
+                        outline_color=Color(a=1.0, b=b, g=g, r=r),
                         fill_color=Color(a=0.1, b=b, g=g, r=r),
-                        thickness=4.
-                        )
+                        thickness=4.0,
+                    )
                 )
 
                 img_annotations.texts.append(
                     TextAnnotation(
                         timestamp=timestamp,
                         text=f"{class_name}: {conf:.2f}",
-                        font_size=35.,
+                        font_size=35.0,
                         position=Point2(x=float(x), y=float(y)),
-                        text_color=Color(a=1., b=b, g=g, r=r),
-                        background_color=Color(a=0., b=b, g=g, r=r)
+                        text_color=Color(a=1.0, b=b, g=g, r=r),
+                        background_color=Color(a=0.0, b=b, g=g, r=r),
                     )
                 )
 
         else:
-            # dummy annotation to keep image annotations in sync with image 
+            # dummy annotation to keep image annotations in sync with image
             img_annotations.texts.append(
                 TextAnnotation(
                     timestamp=timestamp,
                     text="dummy_msg",
-                    font_size=1.,
-                    position=Point2(x=10., y=10.),
-                    text_color=Color(a=0., b=0., g=0., r=0.),
+                    font_size=1.0,
+                    position=Point2(x=10.0, y=10.0),
+                    text_color=Color(a=0.0, b=0.0, g=0.0, r=0.0),
                 )
             )
 
         self.publisher_.publish(img_annotations)
 
-def main():
 
+def main():
     rclpy.init()
-    node = ObjectDetectionNode(min_bbox_area=400, min_bbox_side=25, debug=False)
+    node = ObjectDetectionNode(
+        min_bbox_area=400, min_bbox_side=25, debug=False
+    )
     rclpy.spin(node)
 
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/objectdetection/setup.py
+++ src/objectdetection/setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 
-package_name = 'objectdetection'
+package_name = "objectdetection"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['./model/coco.names']),
-        ('share/' + package_name, ['./model/yolov3.cfg']),
-        ('share/' + package_name, ['./model/yolov3.weights']),
-        
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, ["./model/coco.names"]),
+        ("share/" + package_name, ["./model/yolov3.cfg"]),
+        ("share/" + package_name, ["./model/yolov3.weights"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='nengelmann',
-    maintainer_email='mail@nico-engelmann.de',
-    description='ROS2 package for object detection with YOLOv3',
-    license='MIT',
+    maintainer="nengelmann",
+    maintainer_email="mail@nico-engelmann.de",
+    description="ROS2 node for object detection with YOLOv3",
+    license="MIT",
     entry_points={
-        'console_scripts': [
-            'node = objectdetection.node:main',
+        "console_scripts": [
+            "node = objectdetection.node:main",
         ],
     },
 )
